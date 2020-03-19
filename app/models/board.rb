e9e44e3e40db94224872886cc3033e6274603e33
san_you <pineappleyou1215@yahoo.co.jp>
2020-03-19 21:27:55 +0900

    モデル追加

---
+++ app/models/board.rb
@@ -0,0 +1,4 @@
+class Board < ApplicationRecord
+  has_many :comments
+  has_many :users, through: :comments
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :comments
+  resources :boards
   devise_for :users
   root 'pages#index'
   get 'pages/show'
