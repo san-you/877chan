dee16ff0b1111506a61542d82bae056336780ca1
san_you <pineappleyou1215@yahoo.co.jp>
2020-03-19 22:35:48 +0900

    コメント機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :comments
   resources :boards
   devise_for :users
-  root 'pages#index'
+  root 'boards#index'
   get 'pages/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
