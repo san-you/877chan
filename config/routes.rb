731b2bbbc088d25a84987ef946a6778ecd279b27
san_you <pineappleyou1215@yahoo.co.jp>
2020-03-22 21:57:55 +0900

    登録失敗時のurlを/users/sign_upに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,21 @@
 Rails.application.routes.draw do
   root 'boards#index'
+
   resources :categories
   resources :comments
   resources :boards
-  devise_for :users
+
+  devise_for :users, skip: [:registrations]
+  devise_scope :user do
+    get '/users/cancel', to: 'devise/registrations#cancel', as: :cancel_user_registration
+    get '/users/sign_up', to: 'devise/registrations#new', as: :new_user_registration
+    get '/users/edit', to: 'devise/registrations#edit', as: :edit_user_registration
+    patch '/users', to: 'devise/registrations#update', as: :user_registration
+    put '/users', to: 'devise/registrations#update', as: nil
+    delete '/users', to: 'devise/registrations#delete', as: nil
+    post '/users/sign_up', to: 'devise/registrations#create', as: :post_user_registration
+  end
+
   get 'search/index'
   post 'search/search'
   get 'pages/show'
